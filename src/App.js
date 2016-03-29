e5b3e8cc236e2e9123162ea23a7d44d16ec3b7df
Lyndsey Browning <lbrowning86@gmail.com>
2016-03-29 18:51:49 +0100

    Added cards list sample data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import KanbanBoard from './KanbanBoard';
 
-const Hello = React.createClass({
-  render() {
-    return (
-      <h1> Hello World </h1>
-    );
-  }
-});
+const cardsList = [
+  {
+    id: 1,
+    title: 'Read the Book',
+    description: 'I should read the whole book',
+    status: 'in-progress',
+    tasks: []
+  },
+  {
+    id: 2,
+    title: 'Write some code',
+    description: 'Code along with the samples in the book',
+    status: 'todo',
+    tasks: [
+      {
+        id: 1,
+        name: 'ContactList Example',
+        done: true
+      },
+      {
+        id: 2,
+        name: 'Kanban Example',
+        done: false
+      },
+      {
+        id: 3,
+        name: 'My own experiments',
+        done: false
+      }
+    ]
+  }  
+];
 
-ReactDOM.render(<Hello />, document.getElementById('app'));
+ReactDOM.render(<KanbanBoard cards={cardsList} />, document.getElementById('app'));
