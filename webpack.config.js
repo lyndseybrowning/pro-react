bca79a188817121a63152f62ced60d18a69d09a1
Lyndsey Browning <lbrowning86@gmail.com>
2016-03-30 17:25:08 +0100

    Added full HMR support and .babelrc file

--- webpack.config.js
+++ webpack.config.js
@@ -15,10 +15,7 @@ var config = {
       {
         test: /\.js?$/,
         exclude: /node_modules/,
-        loader: 'babel',
-        query: {
-          presets: ['es2015','react']
-        }
+        loader: 'babel'
       },
       {
         test: /\.css$/,
