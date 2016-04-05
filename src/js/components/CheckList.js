15669439876190f52a1320ef997e4f44128cb832
Lyndsey Browning <lbrowning86@gmail.com>
2016-04-05 18:13:31 +0100

    Bound taskCallback methods

--- src/js/components/CheckList.js
+++ src/js/components/CheckList.js
@@ -7,13 +7,27 @@ export default React.createClass({
     tasks: PropTypes.arrayOf(PropTypes.object)
   },
 
+  checkInputKeyPress(evt) {
+    if(evt.key === 'Enter') {
+      this.props.taskCallbacks.add(this.props.cardId, evt.target.value);
+      evt.target.value = '';
+    }
+  },
+
   render() {
     let cardId = this.props.cardId;
-    let tasks = this.props.tasks.map((task) => (
+    let tasks = this.props.tasks.map((task, taskIndex) => (
       <li className='checklist__task' key={task.id}>
-        <input type='checkbox' defaultChecked={task.done} />
+        <input type='checkbox'
+               defaultChecked={task.done}
+               onChange={
+                 this.props.taskCallbacks.toggle.bind(null, this.props.cardId, task.id, taskIndex)
+               } />
+
         {task.name}
-        <a href='#' className='checklist__task--remove' />
+        <a href='#' className='checklist__task--remove' onClick={
+            this.props.taskCallbacks.delete.bind(null, this.props.cardId, task.id, taskIndex)
+          } />
       </li>
     ));
 
@@ -22,7 +36,8 @@ export default React.createClass({
         <ul>{tasks}</ul>
         <input type='text'
                className='checklist--add-task'
-               placeholder='Type then hit Enter to add a task' />
+               placeholder='Type then hit Enter to add a task'
+               onKeyPress={this.checkInputKeyPress.bind(this)} />
       </div>
     );
   }
